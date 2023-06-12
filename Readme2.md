88edad91755874b5a9742b4b7438117d092c5331
TZARD69 <126488967+TZARD69@users.noreply.github.com>
2023-06-12 15:23:48 +0200

    methode post

---
+++ Readme2.md
@@ -0,0 +1,12 @@
+# Express-03---M-thode-POST
+ce dépôt GitHub contient une route POST pour les utilisateurs.
+Une requête POST sur localhost:5000/api/users devrait créer un nouvel utilisateur dans la base de données avec postman ou autre.
+pour donnée utilise ca sur posteman:
+
+{
+    "firstname": "Jhon",
+    "lastname": "Lartin",
+    "email": "jhon.lartin@example.com",
+    "city": "France",
+    "language": "french"
+}
---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.APP_PORT;
+
+const usersHandlers = require("./usersHandlers");
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite users list");
+};
+
+ app.get("/", welcome);
+
+ app.use(express.json());
+ app.post("/api/users", usersHandlers.postUser);
+
+ app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUsersById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
